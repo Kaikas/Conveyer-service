aa8d9319ef3f7398f05e92a9934323cdd45d00fe
kaikas <kaikasnortheast@googlemail.com>
2018-08-19 14:32:47 +0200

    item ids!

--- distribute.sh
+++ distribute.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo rm -r /var/www/vhosts/sw-gaming.org/eve-service/item.php
-sudo cp -r item.php /var/www/vhosts/sw-gaming.org/eve-service/item.php
+sudo cp -r * /var/www/vhosts/sw-gaming.org/eve-service/
--- item.php
+++ item.php
@@ -1,7 +1,25 @@
 <?php
+//header('Content-Type: application/json');
 
-$json = file_get_contents('https://esi.tech.ccp.is/latest/markets/10000002/orders/?datasource=tranquility&order_type=sell&type_id=16670');
+// Get parameters from url
+$item = $_GET['item'];
+$quantity= $_GET['quantity'];
 
-echo substr($json, 1, -1);
+// Determine best price for item
+$item_prices = json_decode(file_get_contents('https://esi.tech.ccp.is/latest/markets/10000002/orders/?datasource=tranquility&order_type=sell&type_id=' . $item), true);
+$bestprice = $item_prices[0];
+foreach ($item_prices as $price) {
+    if ($bestprice["price"] > $price["price"]) {
+        $bestprice = $price;
+    }
+}
+
+// Get itemname
+$itemnames = file_get_contents('http://sw-gaming.org/eve-service/typeIDs.txt');
+$itemnames_array = explode("\t", $itemnames);
+
+// output
+echo $itemnames_array[0][2];
+echo $bestprice["price"];
 
 ?>
